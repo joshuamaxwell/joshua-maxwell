874c206784d400ea3a71b23336bd79f3106ff01d
JMaxwell <maxwell.joshua@gmail.com>
2022-05-28 23:56:04 -0400

    product name search

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { Product } from '../product';
 import { ProductsService } from '../products.service';
 
@@ -9,6 +9,7 @@ import { ProductsService } from '../products.service';
 })
 export class ProductListComponent implements OnInit {
   products: Product[] = [];
+  @Input() search: string = '';
 
   constructor(
     private productsService: ProductsService
@@ -25,6 +26,15 @@ export class ProductListComponent implements OnInit {
   deleteProduct(id: number) {
     this.productsService.deleteProduct(id).subscribe(res => {
       this.products = this.products.filter(item => item.id !== id);
+    });
+  }
+
+  onSearchTerm(term: string) {
+    console.log(term);
+    this.productsService.getProducts().subscribe(res => {
+      this.products = res.filter(item => {
+        return item.title.toLowerCase().includes(term.toLocaleLowerCase());
+      })
     });
   }
 
